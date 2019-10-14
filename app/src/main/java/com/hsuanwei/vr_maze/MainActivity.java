d288b3d2b08ba4be63ac42b9bc104955043d143e
AlexFxw <alex93179@gmail.com>
2019-10-14 10:56:12 +0800

    init commit.

---
+++ app/src/main/java/com/hsuanwei/vr_maze/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hsuanwei.vr_maze;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+// OpenGL
+import android.opengl.GLES20;
+import android.opengl.Matrix;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
